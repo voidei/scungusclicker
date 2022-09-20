8cfe6499ec2124f4119726f2d9c5e0ea11ef07e3
Dawn Walker <nosirrah.reklaw@gmail.com>
2022-09-21 00:57:13 +1000

    scungus:)

---
+++ changelog.md
@@ -0,0 +1,24 @@
+# Changelog
+
+## 0.1.0
+
+```plaintext
+M U L T I P L I C A T I V E S C U N G I
+```
+
+## 0.0.2
+
+```diff
++ scungus:) now has a changelog
+- scungus:) became slightly uglier
++ scungus:) is now purchaseable
++ scungus:) now tracks your total scungus:) purchases
++ scungus:) now tracks your total scungus:) clicks
++ scungus:) now tells you how much scungus:) costs
+```
+
+## 0.0.1
+
+```diff
++ scungus:) is released to the world
+```
---
+++ scungus.ts
@@ -0,0 +1,47 @@
+/*------------------------ base declarations ------------------------*/
+
+/*---------------------- variable declarations ----------------------*/
+
+var scungusobtainus = 0;	// total number of scungus
+var scunguses = 0;			// number of scungus
+var scungusclicks = 0;		// number of scungus clicked
+var scungusbought = 0;		// number of scungus bought
+var scunguscost = 10;		// cost of scungus
+
+/*---------------------- multiplicative scungi ----------------------*/
+
+/*---------------------------- functions ----------------------------*/
+
+export function clickScungus() {
+	var scungusmultiplicative = 1+(Math.floor(scungusbought*1.5));
+	scunguses += scungusmultiplicative;
+	scungusclicks += 1;
+	scungusobtainus =+ 1;
+
+	document.getElementById("scunguses").innerHTML = scunguses;
+	document.getElementById("scungusbought").innerHTML = scungusbought;
+	document.getElementById("scunguscost").innerHTML = scunguscost;
+	document.getElementById("scungusclicks").innerHTML = scungusclicks;
+
+}
+
+export function buyScungus() {
+	if  (scungusbought <= 0 && scunguses >= scunguscost) {
+		scungusbought += 1;
+		scunguses -= 10;
+		scunguscost == 10;
+		scunguscost = scunguscost+17;
+	}
+
+	if (scunguses >= scunguscost) {
+		scunguses -= scunguscost;
+		scungusbought += 1;
+		scunguscost = scunguscost+17;
+	}
+
+	document.getElementById("scunguses").innerHTML = scunguses;
+	document.getElementById("scungusbought").innerHTML = scungusbought;
+	document.getElementById("scunguscost").innerHTML = scunguscost;
+	document.getElementById("scungusclicks").innerHTML = scungusclicks;
+
+}
